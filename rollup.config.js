6851bbe2a6262266470ee0428c68b6a15c5db734
nischay <nischayrawat100@gmail.com>
2025-03-20 00:54:28 +0530

    plugi

--- rollup.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "rollup";
-import ignore from "rollup-plugin-ignore";
-
-export default defineConfig({
-  plugins: [
-    ignore(["src/socket/marketDataFeed.proto"]),
-  ],
-});
